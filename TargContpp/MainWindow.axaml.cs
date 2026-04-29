fd038f91235a6184de89adfc4e502377159f6ee2
Дмитрий М. <s1ash@mail.ru>
2026-04-29 16:48:46 +0300

    Инициилизация класса MainWindow

--- TargContpp/MainWindow.axaml.cs
+++ TargContpp/MainWindow.axaml.cs
@@ -2,4 +2,9 @@ using Avalonia.Controls;
 using Avalonia.Interactivity;
 using System;
 
-namespace TargetAppAvalonia;
+namespace TargContpp;
+public partial class MainWindow : Window
+{
+    private TextBlock? StatusText;
+    private Button? ShowTimeButton;
+    private TextBlock? TimeText;
